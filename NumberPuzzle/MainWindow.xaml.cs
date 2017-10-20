4ed7bd322af12dc74ca90d2b222d1e8edad733c7
Songjoon Baek <sojbaek@hotmail.com>
2017-10-20 16:13:38 -0400

    Cleaned up some remarks.

--- NumberPuzzle/MainWindow.xaml.cs
+++ NumberPuzzle/MainWindow.xaml.cs
@@ -27,18 +27,6 @@ namespace NumberPuzzle
         {
             InitializeComponent();
 
-            //tbOutput.Text = "Call me Ishmael. Some years ago- never mind how long precisely- having " +
-            //    "little or no money in my purse, and nothing particular to interest me on shore, I thought " +
-            //    "I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. " +
-            //    "Whenever I find myself growing grim  the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing" +
-            //    "before coffin warehouses, and bringing up aboutthe rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it " +
-            //    "requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off- then, " +
-            //    "I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself " +
-            //    "upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, " +
-            //    "cherish very nearly the same feelings towards the ocean with me.\n" + "There now is your insular city of the Manhattoes, belted round by wharves as Indian " +
-            //    "isles by coral reefs-commerce surrounds it with her surf.Right and left, the streets take you waterward.Its extreme downtown is the battery, where that noble mole" +
-            //    " is washed by waves, and cooled by breezes, which a few hours previous were out of sight of land. Look at the crowds of water-gazers there.";
-
             NumberMaze np = new NumberMaze();
             numberMazeCanvas.Doc = np;
            
@@ -58,20 +46,6 @@ namespace NumberPuzzle
             numberMazeCanvas.Velocity = velocitySlider.Value;
             if (np.solved)
             {
-                //StringBuilder stringBuilder = new StringBuilder();
-                //for (int ii = 1; ii < np.Goal; ii++)
-                //{
-                //    StringBuilder s = new StringBuilder();
-                //    foreach (var nn in np.solution[ii])
-                //    {
-                //        int irow = nn / np.cmax;
-                //        int icol = nn % np.cmax;
-                //        s.Append(String.Format("({0},{1}) ", irow, icol));
-                //    }
-                //    stringBuilder.AppendLine(s.ToString());
-                //}
-                //tbOutput.Text = stringBuilder.ToString();
-
                 numberMazeCanvas.draw();
             }
             else
